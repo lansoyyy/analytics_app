d7814888b90a03f6ef7f5635cdc885dd64b5c948
Lans12345 <olanalans12345@gmail.com>
2024-08-01 14:51:34 +0800

    added functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -87,6 +87,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: primary,
         title: TextWidget(
           text: 'HOME',
